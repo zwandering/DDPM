9714f5c2a9e362fc44c35791aff318a0b1f9f7dd
祝昊焜 <1372050977@qq.com>
2023-02-27 14:39:02 +0800

    1

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@
 #DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule cosine --rescale_learned_sigmas False --rescale_timesteps False --use_scale_shift_norm True --resblock_updown True"
 #TRAIN_FLAGS="--lr 2e-5 --batch_size 4"
 path=$0
-OMP_NUM_THREADS=12 torchrun --standalone --nnodes=1 --nproc_per_node=2 train_dist1.py --real_data_dir $1
+OMP_NUM_THREADS=12 torchrun --standalone --nnodes=1 --nproc_per_node=4 train_dist1.py --real_data_dir $1
